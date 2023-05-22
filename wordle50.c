c3e15bc2074b7cdd35896548a4b459861cd69edc
TomasNagy <tomi.nagy@outlook.com>
2023-05-22 20:03:01 +0200

    Implement file reading and randomly chosing a word from the list

--- wordle50.c
+++ wordle50.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+#define LIST_SIZE 1000
 
 bool string_is_number(string text);
 int string_to_int(string number_text);
@@ -20,7 +24,7 @@ int main(int argc, string argv[])
 	// Check if command line argument is a number or has alphabetic letters
 	if(!string_is_number(command_line_input))
 	{
-		printf("Error: wordsize must be either 5, 6, 7, or 8");
+		printf("Error: wordsize must be either 5, 6, 7, or 8\n");
 		return 1;
 	}
 
@@ -29,15 +33,38 @@ int main(int argc, string argv[])
 
 	if (wordsize < 5 || wordsize > 8)
 	{
-		printf("Error: wordsize must be either 5, 6, 7, or 8");
+		printf("Error: wordsize must be either 5, 6, 7, or 8\n");
 		return 1;
 	}
 	
+	// Concatenate the number to the path string
+	char file_name[20];
+	sprintf(file_name, "./word_lists/%i.txt", wordsize);
+
 	// Open the appropriate word list
+	FILE* list_pointer = fopen(file_name, "r");
+
+	// File cannot be opened
+	if(list_pointer == NULL)
+	{
+		printf("Error: file not found!\n");
+		return 1;
+	}
 
 	// Read words into a char array
+	char word_list[LIST_SIZE][wordsize + 1];
+
+	for(int i = 0; i < LIST_SIZE; i++)
+	{
+		fscanf(list_pointer, "%s", word_list[i]);
+	}
 
 	// Select the secret word randomly
+	srand(time(NULL));
+
+	string secret_word = word_list[rand() % LIST_SIZE];
+
+	printf("%s\n", secret_word);
 
 	// Start the game's main loop
 
