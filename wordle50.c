d924128f9b4b55acf7f7fef2a50fa64016ef0e93
TomasNagy <tomi.nagy@outlook.com>
2023-05-22 17:59:45 +0200

    Implement command line argument input checking

--- wordle50.c
+++ wordle50.c
@@ -1,12 +1,38 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
+
+bool string_is_number(string text);
+int string_to_int(string number_text);
 
 int main(int argc, string argv[])
 {
 	// Check if user gave command line argument; if not print error message
+	if(argc != 2)
+	{
+		printf("Usage: ./wordle50 wordsize\n");
+		return 1;
+	}
+
+	string command_line_input = argv[1];
+
+	// Check if command line argument is a number or has alphabetic letters
+	if(string_is_number(command_line_input))
+	{
+		printf("Error: wordsize must be either 5, 6, 7, or 8");
+		return 1;
+	}
 
 	// Get command line argument and conver it to number
+	int wordsize = string_to_int(argv[1]);
 
+	if (wordsize < 5 || wordsize > 8)
+	{
+		printf("Error: wordsize must be either 5, 6, 7, or 8");
+		return 1;
+	}
+	
 	// Open the appropriate word list
 
 	// Read words into a char array
@@ -20,4 +46,35 @@ int main(int argc, string argv[])
 	// Compare the guessed word with the secret word
 
 	// Print informtaion
+}
+
+bool string_is_number(string text)
+{
+	for(int char_index = 0, string_length = strlen(text); char_index < string_length; char_index++)
+	{
+		if(isalpha(text[char_index]))
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int string_to_int(string number_text)
+{
+	int number_int = 0;
+
+	int power_of_ten = 1;
+
+	for(int char_place = strlen(number_text); char_place > 0;)	
+	{
+		char_place--;
+
+		number_int += (number_text[char_place] - 48) * power_of_ten;
+	
+		power_of_ten *= 10;
+	}
+	
+	return number_int;
 }
