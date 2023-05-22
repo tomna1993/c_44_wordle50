6e03fe4db53f6bfa0c086bff92c155258f57e82a
TomasNagy <tomi.nagy@outlook.com>
2023-05-22 17:28:43 +0200

    Add wordle50.c

---
+++ wordle50.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+	// Check if user gave command line argument; if not print error message
+
+	// Get command line argument and conver it to number
+
+	// Open the appropriate word list
+
+	// Read words into a char array
+
+	// Select the secret word randomly
+
+	// Start the game's main loop
+
+	// Get a guess from user as string; get_guess()
+
+	// Compare the guessed word with the secret word
+
+	// Print informtaion
+}
